4b954f3e8b8519bf3eacaf47c988e43ccaa9893a
brandonquinn03-star <brandonquinn03@gmail.com>
2025-09-22 14:29:03 -0400

    Update util.ts

--- lib/util.ts
+++ lib/util.ts
@@ -1,1 +1,5 @@
-export const x = 1;
+export async function fetchJSON(url: string, init?: RequestInit){
+  const res = await fetch(url, { headers:{'Content-Type':'application/json'}, ...(init||{}) })
+  if(!res.ok) throw new Error('Request failed')
+  return res.json()
+}
